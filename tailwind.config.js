9e526875633f1730c8372bb5def0953b782dc311
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2022-10-17 19:46:23 +0200

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        arial: 'arial,sans-serif',
+        arial: ['Arial,sans-serif'],
+      },
+      fontSize: {
+        primarySize: '0.813rem',
+        secondarySize: '0.875rem',
+        extreSize: '0.937rem',
+      },
+      colors: {
+        'primary-color': '#f2f2f2',
+        'secondary-color': '#70757a',
+      },
+      spacing: {
+        mid: '500px',
       },
     },
   },
